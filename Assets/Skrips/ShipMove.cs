f76f9d4b66fa2961d4fca168911057964f53c9d8
theo <theo@zibell.se>
2023-03-08 22:19:52 +0100

    uppdate

--- Assets/Skrips/ShipMove.cs
+++ Assets/Skrips/ShipMove.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class ShipMove : MonoBehaviour
 {
-    Vector3 targetPosition;
+    Vector3 targetPositionG;
+    Vector3 CurentPosition;
+    Vector3 Position;
     Transform transform;
+    [SerializeField] private float Speed = 5f;
+
+    bool Test = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +22,40 @@ public class ShipMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        MoveShipStatic();
     }
 
     public void MoveShip(Vector3 targetPosition)
     {
-        transform.position = targetPosition;
+        targetPosition = targetPositionG;
+        Test = false;
+    }
+
+    public void MoveShipStatic()
+    {
+        if (Test)
+        {
+            CurentPosition = transform.position;
+
+            if (CurentPosition.x > targetPositionG.x)
+            {
+                Position += new Vector3(Speed, 0, 0);
+            }
+            else if (CurentPosition.x < targetPositionG.x)
+            {
+                Position += new Vector3(-Speed, 0, 0);
+            }
+
+            if (CurentPosition.z > targetPositionG.z)
+            {
+                Position += new Vector3(0, 0, Speed);
+            }
+            else if (CurentPosition.z < targetPositionG.z)
+            {
+                Position += new Vector3(0, 0, -Speed);
+            }
+
+            transform.position += Position;
+        }
     }
 }
