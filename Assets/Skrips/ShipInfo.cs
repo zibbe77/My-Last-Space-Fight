434ed4bc7d2f44c8b0855ef00d93ceab4e0f098e
zibbe77 <91542247+zibbe77@users.noreply.github.com>
2023-03-10 19:47:37 +0100

    Ui uppdate

---
+++ Assets/Skrips/ShipInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipInfo : MonoBehaviour
+{
+    //ship stuff
+    public int Hp;
+    public int dmg;
+
+    //resursers 
+    public int Minerals;
+    public int Gas;
+}
